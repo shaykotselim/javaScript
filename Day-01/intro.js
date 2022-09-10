397a64f1b5edddabea99b209ffb4d42f414b45bb
shaykot43500 <hossainshaykot@gmail.com>
2022-09-10 21:18:28 +0600

    first commit in js

---
+++ Day-01/intro.js
@@ -0,0 +1,38 @@
+// Defination of Variable 
+/*
+A symbolic name associated with a value and whose associated value may be changed.
+*/
+// Example : 
+
+// Numeric
+var price = 21;
+var age  = 24;
+var temperature = 37;
+
+/**
+String
+
+A string is a data type used in programming, that is used to represent text rather than numbers
+ */
+
+var myname ="shaykot-selim";
+var address = "battary goli ";
+var schoolName = 'KhanKandi S A Ali High School';
+
+/**
+    Boolean
+
+  A boolean or bool is a data type with two possible values: true or false. 
+ */
+var pass = true;
+var subScribe = false;
+
+// Note:
+/**
+ * ReServed Keyword is not allow;
+ * variable Name should be one word;
+ * variable name in quote is not allowed;
+ * variable name is must be a meanign full word and its represent camelcase;
+ * 
+*/
+ 
